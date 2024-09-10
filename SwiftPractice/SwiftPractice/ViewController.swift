495f9d5182cbd49e8eb4a5a190f5e4c7b5883565
Leo <leo-turko@mail.ru>
2024-09-10 14:48:30 +0300

    feat: Creat App for Swift practice

---
+++ SwiftPractice/SwiftPractice/ViewController.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+class ViewController: UIViewController {
+  
+  let stackView = UIStackView()
+  let label = UILabel()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    style()
+    layout()
+  }
+}
+
+extension ViewController {
+  func style() {
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    stackView.axis = .vertical
+    stackView.spacing = 20
+    
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.text = "Dummy"
+    label.font = UIFont.preferredFont(forTextStyle: .title1)
+  }
+  
+  func layout() {
+    stackView.addArrangedSubview(label)
+    
+    view.addSubview(stackView)
+    
+    NSLayoutConstraint.activate([
+      stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+    ])
+  }
+}
+
